b35188a39a449ba86c09b1b7a14bc7e3a95a9301
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-02-28 11:29:33 +0700

    W03: Jawaban soal 3

--- 03-belajar-komponen/src/app/page.tsx
+++ 03-belajar-komponen/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Profile from "../components/profile";
+import { Gallery } from "@/components/gallery";
+import Todolist from "@/components/todolist";
+import Bio from "@/components/bio";
+import Avatar from "@/components/avatar";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <p className="justify-center mt-16">Ilmuwan yang luar biasa</p>
+      {/* <p className="justify-center mt-16">Ilmuwan yang luar biasa</p>
 
       <div className="flex mt-32">
         <Profile />
@@ -12,7 +16,14 @@ export default function Home() {
           <Profile />
         </div>
         <Profile />
-      </div>
+      </div> */}
+
+      <Todolist />
+
+      {/* <Bio />
+      <Gallery /> */}
+
+      {/* <Avatar /> */}
     </main>
   );
 }
---
+++ 03-belajar-komponen/src/components/bio.tsx
@@ -0,0 +1,18 @@
+export default function Bio() {
+  return (
+    <div>
+      <div className="intro">
+        <h1>HAI... Selamat datang di website saya!</h1>
+      </div>
+      <p className="summary">
+        Anda dapat membaca mumetku di sini.
+        <br />
+        <br />
+        <b>
+          Juga ada <i>foto</i>
+        </b>{" "}
+        ilmuwan!
+      </p>
+    </div>
+  );
+}
