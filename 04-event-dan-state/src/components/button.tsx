9c97156691cc94b69edbca41f9fc02ada553686d
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-03-06 09:46:02 +0700

    Praktikum 3

--- 04-event-dan-state/src/components/button.tsx
+++ 04-event-dan-state/src/components/button.tsx
@@ -1,8 +1,22 @@
-export function Tombol_2({isiPesan, namaTombol1}) {
+export function Tombol_2({isiPesan, namaTombol1}:any) {
     return(
         <button
             className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
             onClick={() => alert(isiPesan)}>
+            {namaTombol1}
+        </button>
+    );
+}
+
+export function Tombol_3({isiPesan, namaTombol1}:any) {
+    return(
+        <button
+            className="bg-green-500 hover:bg-green-700 text-white p-2 rounded m-2"
+            onClick={(e) => {
+                e.stopPropagation();
+                alert(isiPesan)
+                }   
+            }>
             {namaTombol1}
         </button>
     );
