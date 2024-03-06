e68a63bb6456081ca936d8b36d5186ff08fb5da4
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-03-06 08:57:15 +0700

    Praktikum 1: Event Handler

---
+++ 04-event-dan-state/src/components/button.tsx
@@ -0,0 +1,21 @@
+export default function Tombol_1() {
+    function handleClick() {
+        alert("Taraaa Mak Jrengg!!!");
+    }
+
+    function handleMouseOver() {
+        alert("Eits, tidakk bisaa!!!");
+    }
+    
+    return(
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={handleClick}
+        // onMouseOver={handleMouseOver}
+        onMouseLeave={() => {
+            alert("Loh, kok sudah pergi!!!")
+        }}
+        >
+            Tombol Rahasia
+        </button>
+    );
+}
