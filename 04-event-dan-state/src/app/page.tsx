1014fdec5eca5f5fa74d30b4a9351c327c9554f6
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-03-06 09:38:47 +0700

    Praktikum 2

--- 04-event-dan-state/src/app/page.tsx
+++ 04-event-dan-state/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Tombol_1 from "@/components/button";
+import Tombol_1, { Tombol_2 } from "@/components/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +8,8 @@ export default function Home() {
       <div className="container mx-auto">
         <h2>Kuis Kota</h2>
         <Tombol_1 />
+        <hr></hr>
+        <Tombol_2 isiPesan="Ini Pesanku" namaTombol1="Pesan" />
       </div>
     </>
   );
--- 04-event-dan-state/src/components/button.tsx
+++ 04-event-dan-state/src/components/button.tsx
@@ -1,3 +1,13 @@
+export function Tombol_2({isiPesan, namaTombol1}) {
+    return(
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={() => alert(isiPesan)}>
+            {namaTombol1}
+        </button>
+    );
+}
+
 export default function Tombol_1() {
     function handleClick() {
         alert("Taraaa Mak Jrengg!!!");
