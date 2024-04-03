436e86604695b119cb9af41c9d22caf3c0b89a3b
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-04-03 10:09:53 +0700

    W07: Jawaban soal 2

--- 07-belajar-context-nextjs/src/components/atoms/section.tsx
+++ 07-belajar-context-nextjs/src/components/atoms/section.tsx
@@ -1,3 +1,15 @@
-export default function Section({ children }: { children: any }) {
-  return <section className="section">{children}</section>;
+import { LevelContext } from "@/utilities/context/mycontext";
+
+export default function Section({
+  level,
+  children,
+}: {
+  level: number;
+  children: any;
+}) {
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    </section>
+  );
 }
